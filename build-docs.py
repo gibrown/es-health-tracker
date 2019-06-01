1932e58fb69d4d6e6fb07dfd0b0d1250dc64df77
Greg Ichneumon Brown <gbrown5878@gmail.com>
2019-06-01 15:51:37 -0600

    Building docs working for apple data, symple, and hrv

--- build-docs.py
+++ build-docs.py
@@ -3,6 +3,7 @@
 import sys
 import csv
 import json
+from datetime import *
 
 if ( len(sys.argv) < 2 ):
     sys.exit( 'need to specify csv' )
@@ -15,18 +16,140 @@ comma_flds = {
     "Status"
 }
 
-with open(csv_fname, 'rb' ) as csvfile:
+appl_use_duration = {
+    "AppleStandHour",      #const string
+    "HighHeartRateEvent",  #no value
+    "MindfulSession",      #no value
+    "SleepAnalysis"        #diff strings for status, start with HKCategoryValueSleepAnalysis
+}
+
+hrv_rename = {
+    "HRV": "HRV",
+    "Morning Readiness": "HRV_readiness",
+    "HRV CV": "HRV_CV",
+    "HR": "HRV_HR",
+    "lnRmssd": "HRV_lnRmssd",
+    "Rmssd": "HRV_Rmssd",
+    "Nn50": "HRV_Nn50",
+    "Pnn50": "HRV_Pnn50",
+    "Sdnn": "HRV_Sdnn",
+    "Low Frequency Power": "HRV_low_power",
+    "High Frequency Power": "HRV_hi_power",
+    "LF/HF Ratio": "HRV_power_ratio",
+    "Total Power": "HRV_total_power"
+}
+
+symple_rename = {
+    "0 Walking problems": "Walking problems",
+    "1 Legs burning": "Legs Burning",
+    "2 Muscle weakness": "Muscle Weakness",
+    "3 Hip sore": "Hip Sore",
+    "4 Spacisity": "Spacisity",
+    "5 Poor balance": "Poor Balance",
+    "6 Pain level 1-5+": "Pain Level",
+    "7 Depressed 1-5+": "Depressed",
+    "Almost fell": "Almost fell",
+    "Back (low) pain": "Back pain",
+    "Bad sleep": "Bad sleep",
+    "Bowel discomfort": "Bowel discomfort",
+    "CNS Fatigue tired": "CNS Fatigue tired",
+    "Difficulty concentrating": "Difficulty concentrating",
+    "Fell": "Fell",
+    "Hand tingling/pain": "Hand tingling",
+    "Headache": "Headache",
+    "Insomnia": "Insomnia",
+    "Leg stiffness": "Leg stiffness",
+    "Nausea/dizzy ": "Nausea/dizzy",
+    "Shoulder Soreness": "Shoulder Soreness",
+    "Socks on feet": "Socks on feet",
+    "Stressed": "Stressed",
+    "Weakness": "Weakness",
+    '""Exercise""': "Exercise",
+}
+
+appl_file = False
+symple_file = False
+hrv_file = False
+p1 = False
+with open(csv_fname, 'r' ) as csvfile:
     rdr = csv.DictReader(csvfile, delimiter=",", quotechar='"', escapechar='\\', doublequote=False)
+    if ( 'sourceName' in rdr.fieldnames ):
+        appl_file = True
+    if ( 'Period' in rdr.fieldnames ):
+        symple_file = True
+    if ( 'Pnn50' in rdr.fieldnames ):
+        hrv_file = True
     for row in rdr:
         doc = {}
-        for key, value in row.items():
-            v = value
-            if ( key in comma_flds ):
-            	if not value:
-                    v = []
-                else:
-	            v = value.split( ', ' )
-            doc[key] = v
 
-        print json.dumps(doc, separators=(',',':'))
+        if ( appl_file ):
+            s = datetime.strptime( row["startDate"][0:-6], "%Y-%m-%d %H:%M:%S" )
+            e = datetime.strptime( row["endDate"][0:-6], "%Y-%m-%d %H:%M:%S" )
+            doc['Timestamp'] = s.strftime( '%Y-%m-%d %H-%M-%S' )
+            doc['duration'] = (e - s).total_seconds()
+            if ( row['type'] in appl_use_duration ):
+                fld = row['type']
+                if ( row['type'] == 'SleepAnalysis' ):
+                    fld = fld + '_' + row['value'].replace( 'HKCategoryValueSleepAnalysis', '' )
+                doc[fld] = doc['duration']
+            else:
+	        fld = row['type'] + '_' + row['unit']
+	        doc[fld] = float( row['value'] )
+	        if ( doc['duration'] > 0 ):
+	            doc[fld + '_rate'] = float( row['value'] ) / doc['duration']
+            if ( not p1 ):
+                sys.stderr.write( fld + "\n" )
+                sys.stderr.write( fld + "_rate\n" )
+                p1 = True
+
+        elif ( symple_file ):
+            d = datetime.strptime( row['Date'], '%Y-%m-%d' )
+            if ( 'nite' == row['Period'] ):
+                d += timedelta(hours=4)
+            elif ( 'am' == row['Period'] ):
+                d += timedelta(hours=8)
+            elif ( 'mid' == row['Period'] ):
+                d += timedelta(hours=14)
+            elif ( 'pm' == row['Period'] ):
+                d += timedelta(hours=20)
+            doc['Timestamp'] = d.strftime( '%Y-%m-%d %H-%M-%S' )
+
+            blank = True
+            for key, value in row.items():
+                if ( key in symple_rename ):
+                    if ( not value ):
+                    	v = 0
+                    else:
+                    	v = int( value )
+                    if ( v > 0 ):
+                        blank = False
+                        doc[symple_rename[key]] = v
+            if ( blank ):
+                continue   #blank doc
+
+        elif ( hrv_file ):
+            if ( 'readiness' != row['Type'] ):
+                continue
+            d = datetime.strptime( row["Date Time Start"], "%Y-%m-%d %H:%M:%S" )
+	    doc['duration'] = int( round( float( row["Duration"] ) ) )
+            doc['Timestamp'] = d.strftime( '%Y-%m-%d %H-%M-%S' )
+            for key, value in row.items():
+                if ( key in hrv_rename ):
+                    if ( not value ):
+                    	v = 0
+                    else:
+                    	v = float( value )
+                    doc[hrv_rename[key]] = v
+
+        else:
+            for key, value in row.items():
+                v = value
+                if ( key in comma_flds ):
+                    if not value:
+                        v = []
+                    else:
+                        v = value.split( ', ' )
+                doc[key] = v
+
+        print( json.dumps(doc, separators=(',',':')) )
 
--- convert-to-json.sh
+++ convert-to-json.sh
@@ -9,8 +9,44 @@ FILES=(
 	Bowels
 )
 
+APPLFILES=(
+	ActiveEnergyBurned
+	AppleExerciseTime
+	AppleStandHour
+	BasalEnergyBurned
+	BodyFatPercentage
+	BodyMass
+	BodyMassIndex
+	DistanceSwimming
+	DistanceWalkingRunning
+	FlightsClimbed
+	HeartRate
+	HeartRateVariabilitySDNN
+	HighHeartRateEvent
+	MindfulSession
+	RespiratoryRate
+	RestingHeartRate
+	SleepAnalysis
+	StepCount
+	SwimmingStrokeCount
+	WalkingHeartRateAverage
+
+#skip - different format - maybe redundant
+#	ActivitySummary
+#	Workout
+)
 
 for F in ${FILES[*]}
 do
 	python build-docs.py "$CSVPATH/$F.csv" > "$JSONPATH/$F.json"
+done
+
+python build-docs.py "$CSVPATH/symple.csv" > "$JSONPATH/symple.json"
+
+python build-docs.py "$CSVPATH/hrv.csv" > "$JSONPATH/hrv.json"
+
+for F in ${APPLFILES[*]}
+do
+	echo "$F"
+	python build-docs.py "$CSVPATH/apple_health_export/$F.csv" > "$JSONPATH/$F.json"
 done
